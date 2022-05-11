739b34f1d686533d112bac4c011de8c529225a4d
Douglas Campbell <dacampbelltbone@gmail.com>
2022-05-11 00:12:02 -0400

    First Commit. Able to make purchases in SOL. Able to display dollar amount with USDC and make purchases in USDC.

---
+++ lib/addresses.ts
@@ -0,0 +1,4 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const shopAddress = new PublicKey('Fux878qLnMuVbT2prwicqAuuth3wM4zTyAY5KNgjuxjf')
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
